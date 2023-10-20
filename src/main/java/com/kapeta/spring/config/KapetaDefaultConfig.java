ca30e94398ada33995dfff2940d8040ab5c6c104
Henrik Hofmeister <henrik@newdawn.dk>
2023-10-20 22:49:47 +0200

    fix: Use default mapper for reading config
    More lenient - ignores unknown properties and treats dates as longs

--- src/main/java/com/kapeta/spring/config/KapetaDefaultConfig.java
+++ src/main/java/com/kapeta/spring/config/KapetaDefaultConfig.java
@@ -1,9 +1,13 @@
 package com.kapeta.spring.config;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -12,6 +16,16 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  * Default configuration for kapeta
  */
 public class KapetaDefaultConfig {
+
+    public static ObjectMapper createDefaultObjectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+                .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .build();
+    }
 
 
     /**
@@ -26,12 +40,7 @@ public class KapetaDefaultConfig {
     @Bean
     @ConditionalOnMissingBean(ObjectMapper.class)
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.findAndRegisterModules();
-        mapper.disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
-        mapper.disable(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS);
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        return mapper;
+        return createDefaultObjectMapper();
     }
 
     @Bean
