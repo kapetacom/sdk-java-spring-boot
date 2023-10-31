aa4ab9ea1a1ac3d549f83280e630d6f712dd323c
Morten Holm <mohoholm@gmail.com>
2023-10-31 08:46:27 +0100

    KAP-1755 Redirect root to open api documentation
    Add: Controller that redirects "/" to swagger openapi documentation (enabled by default, toggled by property 'kapeta.openapi-redirect')

---
+++ src/main/java/com/kapeta/spring/rest/OpenAPIRedirectController.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: MIT
+ */
+package com.kapeta.spring.rest;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class OpenAPIRedirectController {
+
+    @GetMapping("/")
+    @Hidden
+    public RedirectView redirectToSwaggerDocumentation() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+}
