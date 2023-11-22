acb79174a21d4e70c1f444745375a85f4a0608be
Morten Holm <mohoholm@gmail.com>
2023-11-22 15:52:26 +0100

    KAP-2037 Add tracing
    fix: clean up pom.xml
    add: micrometer tracing now included

--- src/main/java/com/kapeta/spring/annotation/KapetaSpringApplication.java
+++ src/main/java/com/kapeta/spring/annotation/KapetaSpringApplication.java
@@ -7,12 +7,6 @@ package com.kapeta.spring.annotation;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -24,9 +18,7 @@ import java.lang.annotation.*;
 @Documented
 @Inherited
 @SpringBootConfiguration
-@EnableAutoConfiguration(
-
-)
+@EnableAutoConfiguration
 @ComponentScan(excludeFilters = {
         @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class)
 })
